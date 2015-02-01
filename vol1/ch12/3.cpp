2cf69fcd9b691309cf2f87058dff4456d8ebe5ef
petea <smart__petea@mail.ru>
2015-02-01 19:23:25 +0200

    v1.ch12.3

---
+++ vol1/ch12/3.cpp
@@ -0,0 +1,49 @@
+/*
+ * Add a binary operator- to Exercise 2 as a member
+ * function. Demonstrate that you can use your objects in 
+ * complex expressions like
+ * a + b - c
+ */
+
+#include <ostream>
+#include <iostream>
+
+class A
+{
+	private:
+		int x_;
+
+	public:
+		A(int x):x_(x) {};
+
+		A operator+(A const& a1) const
+		{
+			return A(x_ + a1.x_);
+		}
+
+		A operator-(A const& a1) const
+		{
+			return A(x_ - a1.x_);
+		}
+
+		void print(std::ostream& ostr) 
+		{
+			ostr << "x = " << x_ << "\n";
+		}
+};
+
+int main()
+{
+	A a(5);
+	A b(4);
+	A c(2);
+
+	A d = a + b -c - c ;
+
+	a.print(std::cout);
+	b.print(std::cout);
+	c.print(std::cout);
+	d.print(std::cout);
+
+	return 0;
+}
