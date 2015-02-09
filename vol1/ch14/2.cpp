93aeaa899a951d6266c89dcd311cec6a44420424
smart__petea <smart__petea@mail.ru>
2015-02-09 20:42:07 +0200

    v1.ch14.2

---
+++ vol1/ch14/2.cpp
@@ -0,0 +1,40 @@
+/*
+ * Create two classes, A and B, with default constructors
+ * that announce themselves. Inherit a new class called C
+ * from A, and create a member object of B in C, but do not
+ * create a constructor for C. Create an object of class C and
+ * observe the results.
+ */
+
+#include <iostream>
+using namespace std;
+
+class A
+{
+    public:
+        A()
+        {
+            cout << "A()" << endl;
+        }
+};
+
+class B
+{
+    public:
+        B()
+        {
+            cout << "B()" << endl;
+        }
+};
+
+class C : public A
+{
+    private:
+        B b;
+};
+
+int main()
+{
+    C c;
+}
+
