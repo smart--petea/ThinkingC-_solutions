4a8ab7d2c731eec776fcca48a7049201fe45e2c0
smart__petea <smart__petea@mail.ru>
2015-02-11 20:13:39 +0200

    v1.ch14.10

---
+++ vol1/ch14/10.cpp
@@ -0,0 +1,18 @@
+/*
+ * Write a class containing a long and use the pseudoconstructor 
+ * call syntax in the constructor to initialize the long
+ */
+
+class A
+{
+        long x;
+    public:
+        A() : x(5) {};
+};
+
+int main()
+{
+    A a;
+
+    return 0;
+}
